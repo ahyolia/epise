07d13e5c646e00fd550f058e9d9f39a4666661cf
Camélia AMIN HANDOYO <camhdyo@gmail.com>
2025-07-09 11:00:19 +1100

    Add files via upload

---
+++ app/Debug.php
@@ -0,0 +1,18 @@
+<?php 
+    namespace app;
+
+    class Debug{
+        public static bool $actif = false;
+        public static function debug(array $array): void {
+        
+        if(!self::$actif) return;
+            echo "<pre class='debug'>";
+            print_r($array);
+            echo "</pre>";
+        }
+        public static function debugDie(array $array): void {
+            self::debug($array);
+            die();
+        }
+    }
+?>
