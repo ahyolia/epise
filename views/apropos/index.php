fbbd1d7c08c2304ee830b7f31fbd35cd2d241bc4
Camélia AMIN HANDOYO <camhdyo@gmail.com>
2025-07-09 11:06:21 +1100

    Add files via upload

---
+++ views/apropos/index.php
@@ -0,0 +1,93 @@
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>À propos de l'EPISE</title>
+  <link rel="stylesheet" href="/css/styleapropos.css">
+  <script defer src="/js/caroussel.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Poppins:wght@600&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@500;600&display=swap" rel="stylesheet">
+</head>
+<body>
+<section class="presentation">
+  <div class="background-pattern"></div>
+  <div class="content">
+    <div class="image">
+      <img src="../../images/MDE_LOCAL.jpg" alt="Présentation de l'EPISE">
+    </div>
+    <div class="text">
+      <h2>Présentation</h2>
+      <p>L’Épicerie Solidaire de l’UNC (EPISE) est une association gérée par des étudiants de l'université, qui soutient les étudiants du campus en leur proposant toutes sortes de produits alimentaires, hygièniques et autres matériels de première nécessité, et tout ça seulement à hauteur de 200f par an.</p>
+      <p>Elle vise à améliorer leur quotidien tout en luttant contre la précarité étudiante.</p>
+      <p>Des actions solidaires et collectives y sont organisées régulièrement. Mais ce n'est pas assez, l'EPISE souhaite être ouverte plus souvent et pour cela l'équipe de bénévoles doit s'agrandir.</p>
+    </div>
+  </div>
+  </section>
+
+  <section class="histoire">
+    <div class="text">
+      <h2>Histoire</h2>
+      <p>L'EPISE à été créée par Mme Brigitte Gustin et l'ancien directeur de la MDE.<br>
+Le directeur était sorti de son bureau et il a vu un étudiant qui attendait sur les canapé de l'accueil de la MDE et d'un coup l'étudiant s'est effondré ! <br>
+Le directeur est directement intervenu et lui a demandé si ça allait bien et l'a questionné un peu jusqu'à ce que l'étudiant lui confie que ça faisait UNE SEMAINE qu'il n'avait pas mangé.<br>
+Et là il s'est dit que ce n’était pas possible, qu’il devait faire quelque chose. </p>
+<p>Alors il a contacté Mme Gustin et ils ont créé l'EPISE, dont la particularité est qu'elle ne peut être gérée que par les étudiants eux-mêmes, pour inciter les jeunes à s’entraider et améliorer la qualité de vie dans leur campus malgré les difficultés.
+</p>
+
+    </div>
+    <div class="image">
+      <img src="../../images/sad.jpg" alt="Présentation de l'EPISE">
+    </div>
+  </section>
+
+  <section class="localisation">
+    <div class="map">
+       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2167.020818646322!2d166.40271211030625!3d-22.26235086669137!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6c27e3194b0b193b%3A0x1342676e44ff8345!2s145%20Ave%20James%20Cook%2C%20Noum%C3%A9a%2098800%2C%20Nouvelle-Cal%C3%A9donie!5e0!3m2!1sfr!2sfr!4v1751515476402!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+    </div>
+    <div class="image">
+      <img src="../../images/plan.jpg" alt="Présentation de l'EPISE">
+    </div>
+    <div class="text">
+      <h2>Où se situe l’Epise ?</h2>
+      <p>L’EPISE est située sur le campus de l’Université de la Nouvelle-Calédonie, à côté de la MDE et du Sunset Café, juste en bas des escaliers à côté des terrains de sport.</p>
+      <p>Elle est facilement accessible aux étudiants et ouverte deux jours par semaine.</p>
+    </div>
+  </section>
+
+<section class="quote-section">
+  <div class="quote-overlay"><img src="../../images/benevole2.jpg" alt="guillemet gauche"></div>
+  <div class="quote-content">
+    <img src="../../images/guillemet-gauche.png" alt="guillemet gauche" class="quote-icon left">
+    
+    <blockquote>
+      <p class="quote-text">
+        Aucun objectif n’est trop grand,<br>
+        pour qui y met son cœur
+      </p>
+      <cite class="quote-author">AUVAA TIFAINE <br> Ancienne présidente de l'EPISE</cite>
+    </blockquote>
+
+    <img src="../../images/guillemet-droit.png" alt="guillemet droit" class="quote-icon right">
+  </div>
+</section>
+  
+<section class="membres-bureau">
+  <h2>Membres du bureau de l’épise</h2>
+
+  <div class="membres-container">
+    <div class="membre">
+      <img src="../../images/profil1.png" alt="Avatar" />
+      <p><strong>Yvona SCHOUENE</strong><br><span>Secrétaire</span></p>
+    </div>
+    <div class="membre">
+      <img src="../../images/profil2.png" alt="Avatar" />
+      <p><strong>Noémie DE SAINT QUENTIN</strong><br><span>Trésorière</span></p>
+    </div>
+    <div class="membre">
+      <img src="../../images/profil3.png" alt="Avatar" />
+      <p><strong>Falakika MUNI</strong><br><span>Présidente</span></p>
+    </div>
+  </div>
+</section>
+</body>
